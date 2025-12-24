773101d96e46e1497925f75a8864732a4b5db75f
Srushti-Shedshale-1008 <srushtishedshale1008@gmail.com>
2025-12-25 00:09:58 +0530

    Reversing a String

--- Day01_Arrays/README.md
+++
@@ -1,1 +0,0 @@
-Day 1 – Array basics problems
---
+++ Day03_Strings/ReverseString.java
@@ -0,0 +1,11 @@
+package Day03_Strings;
+public class ReverseString {
+    public static void main(String[] args) {
+        String str="Reverse";
+        String reversedStr="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversedStr=reversedStr+str.charAt(i);
+        }
+        System.out.println("Original String:"+str+"\nReversed String:"+reversedStr);
+    }
+}
