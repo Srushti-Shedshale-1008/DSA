ab3d75b6b3e596abefc5ee0f3c2d7502d6790d43
Srushti-Shedshale-1008 <srushtishedshale1008@gmail.com>
2025-12-31 00:58:03 +0530

    Reverse the given number

---
+++ Day09/ReverseNumber.java
@@ -0,0 +1,16 @@
+//Reversing a number
+package Day09;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num=130980;
+        int rev=0;
+        while(num>0){
+            int rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println("\nReversed Number:"+rev);
+    }
+    
+}
