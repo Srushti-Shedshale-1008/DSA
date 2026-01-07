fcff6cf34bef5af6f501fb7cc2b3e7dc65e5172d
Srushti-Shedshale-1008 <srushtishedshale1008@gmail.com>
2026-01-07 07:10:50 +0530

    Power of digit using recursion

---
+++ Day13_RecursionBasic/PowerUsingRecursion.java
@@ -0,0 +1,19 @@
+//Power of digit using recursion
+package Day13_RecursionBasic;
+
+public class PowerUsingRecursion {
+    public int power(int n,int p){
+        if(p==0){
+            return 1;
+        }
+        /*LOGIC: 
+        - Reducing the power by 1 till power==0 (base case).
+        - Multiplying by the n each time with result of smaller subproblem*/
+        return n* power(n, p-1);
+    }
+    public static void main(String[] args) {
+        PowerUsingRecursion pow=new PowerUsingRecursion();
+        int powerIs=pow.power(3, 3);
+        System.out.println("\n:" +powerIs);
+    }
+}
